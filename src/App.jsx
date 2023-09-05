27404d56d06d10175981ccb5c666f9ba68204bd9
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-09-05 20:44:05 +0800

    Add header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Sidebar from './components/Sidebar';
 import GeneralInfo from './components/GeneralInfo';
 import EducationalExp from './components/EducationalExp';
 import PracticalExp from './components/PracticalExp';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import './App.css'
 
 function App() {
@@ -16,25 +18,35 @@ function App() {
     address: 'Mabalacat City',
     mobile: '12345678910',
     email: 'abc@email.com',
-    skills: [],
-    languages: []
+    skills: ['Skill1', 'Skill2'],
+    languages: ['Language1']
   })
 
   return (
-    <div className='row'>
-      <div className='main'>
-        <div className="main-row">
-          <div className='main-left-col'>
-            <GeneralInfo info={info} setInfo={setInfo}/>
+    <>
+      <header>Resume creator</header>
+      <div className="main">
+        <main className='resume'>
+          <div className="resume-row">
+            <div className='resume-left-col'>
+              <GeneralInfo info={info} setInfo={setInfo}/>
+            </div>
+            <div className="resume-right-col">
+              <PracticalExp />
+              <EducationalExp />
+            </div>
           </div>
-          <div className="main-right-col">
-            <PracticalExp />
-            <EducationalExp />
-          </div>
-        </div>
+        </main>
+        <Sidebar info={info} setInfo={setInfo} />
       </div>
-      <Sidebar info={info} setInfo={setInfo} />
-    </div>
+      <footer>
+        Created by Jarrell21
+        <a href="https://github.com/Jarrell21" target="_blank" rel="noreferrer">
+            {/* <i className="fa-brands fa-github"></i> */}
+            <FontAwesomeIcon icon={faGithub} />
+        </a>
+    </footer>
+    </>
   )
 }
 
