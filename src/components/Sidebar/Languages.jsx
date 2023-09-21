7dd0a071385cf9cd8b031876700d750d4c51cdc5
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-09-21 22:12:44 +0800

    Add skill and language CRUD functionalities

--- src/components/Sidebar/Languages.jsx
+++ src/components/Sidebar/Languages.jsx
@@ -1,17 +1,134 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
 
 function Languages({ data, setData }) {
-  const handleAddLanguage = () => {};
+  const [mode, setMode] = useState({
+    action: "",
+    languageId: "",
+    languageName: "",
+  });
+
+  const addLanguage = () => {
+    setMode({ action: "add", languageId: "", languageName: "" });
+  };
+
+  const editLanguage = (languageId, languageName) => {
+    setMode({
+      action: "edit",
+      languageId: languageId,
+      languageName: languageName,
+    });
+  };
+
+  const exitInput = () => {
+    const languageInput = document.getElementById("language-input");
+
+    languageInput.value = "";
+    setMode("");
+  };
+
+  const saveNewLanguage = () => {
+    const languageName = document.getElementById("language-input").value;
+    const languageId = uuidv4();
+
+    setData({
+      ...data,
+      personalInfo: {
+        ...data.personalInfo,
+        languages: [
+          ...data.personalInfo.languages,
+          { id: languageId, languageName: languageName },
+        ],
+      },
+    });
+
+    exitInput();
+  };
+
+  const saveEditedLanguage = (languageId) => {
+    const languageName = document.getElementById("language-input").value;
+    const newlanguages = [...data.personalInfo.languages].map((language) => {
+      if (language.id == languageId) {
+        language.languageName = languageName;
+      }
+
+      return { id: language.id, languageName: language.languageName };
+    });
+
+    setData({
+      ...data,
+      personalInfo: {
+        ...data.personalInfo,
+        languages: newlanguages,
+      },
+    });
+
+    exitInput();
+  };
+
+  const deleteLanguage = (languageId) => {
+    const newlanguages = [...data.personalInfo.languages].filter(
+      (language) => language.id != languageId
+    );
+
+    setData({
+      ...data,
+      personalInfo: {
+        ...data.personalInfo,
+        languages: newlanguages,
+      },
+    });
+
+    exitInput();
+  };
 
   return (
     <div className="side-languages">
-      <h3>Languages</h3>
-      {data.personalInfo.languages.map((language) => (
-        <p key={language.id} className="language">
-          {language.languageName}
-        </p>
-      ))}
-      <button onClick={handleAddLanguage}>Add language</button>
+      <h3>languages</h3>
+      {mode.action == "add" ? (
+        <div className="add-language">
+          <input
+            className="input"
+            id="language-input"
+            name="language"
+            type="text"
+          />
+          <button onClick={saveNewLanguage}>Save</button>
+          <button onClick={exitInput}>Cancel</button>
+        </div>
+      ) : mode.action == "edit" ? (
+        <div className="edit-language">
+          <input
+            className="input"
+            id="language-input"
+            name="language"
+            type="text"
+            defaultValue={mode.languageName}
+          />
+          <button onClick={() => saveEditedLanguage(mode.languageId)}>
+            Save
+          </button>
+          <button onClick={() => deleteLanguage(mode.languageId)}>
+            Delete
+          </button>
+          <button onClick={exitInput}>Cancel</button>
+        </div>
+      ) : (
+        <>
+          {data.personalInfo.languages.map((language) => (
+            <p
+              key={language.id}
+              className="language"
+              data-language-id={language.id}
+              onClick={() => editLanguage(language.id, language.languageName)}
+            >
+              {language.languageName}
+            </p>
+          ))}
+          <button onClick={addLanguage}>Add language</button>
+        </>
+      )}
     </div>
   );
 }
--- src/components/Sidebar/Skills.jsx
+++ src/components/Sidebar/Skills.jsx
@@ -3,21 +3,25 @@ import PropTypes from "prop-types";
 import { v4 as uuidv4 } from "uuid";
 
 function Skills({ data, setData }) {
-  const [addSkillMode, setaddSkillMode] = useState(false);
+  const [mode, setMode] = useState({ action: "", skillId: "", skillName: "" });
 
-  const handleAddSkill = () => {
-    setaddSkillMode((prev) => !prev);
+  const addSkill = () => {
+    setMode({ action: "add", skillId: "", skillName: "" });
   };
 
-  const handleCancelAddSkill = () => {
-    const newSkillInput = document.getElementById("new-skill");
-
-    newSkillInput.value = "";
-    setaddSkillMode((prev) => !prev);
+  const editSkill = (skillId, skillName) => {
+    setMode({ action: "edit", skillId: skillId, skillName: skillName });
   };
 
-  const handleSaveSkill = () => {
-    const skillValue = document.getElementById("new-skill").value;
+  const exitInput = () => {
+    const skillInput = document.getElementById("skill-input");
+
+    skillInput.value = "";
+    setMode("");
+  };
+
+  const saveNewSkill = () => {
+    const skillName = document.getElementById("skill-input").value;
     const skillId = uuidv4();
 
     setData({
@@ -26,32 +30,87 @@ function Skills({ data, setData }) {
         ...data.personalInfo,
         skills: [
           ...data.personalInfo.skills,
-          { id: skillId, skillName: skillValue },
+          { id: skillId, skillName: skillName },
         ],
       },
     });
 
-    handleCancelAddSkill();
+    exitInput();
+  };
+
+  const saveEditedSkill = (skillId) => {
+    const skillName = document.getElementById("skill-input").value;
+    const newSkills = [...data.personalInfo.skills].map((skill) => {
+      if (skill.id == skillId) {
+        skill.skillName = skillName;
+      }
+
+      return { id: skill.id, skillName: skill.skillName };
+    });
+
+    setData({
+      ...data,
+      personalInfo: {
+        ...data.personalInfo,
+        skills: newSkills,
+      },
+    });
+
+    exitInput();
+  };
+
+  const deleteSkill = (skillId) => {
+    const newSkills = [...data.personalInfo.skills].filter(
+      (skill) => skill.id != skillId
+    );
+
+    setData({
+      ...data,
+      personalInfo: {
+        ...data.personalInfo,
+        skills: newSkills,
+      },
+    });
+
+    exitInput();
   };
 
   return (
     <div className="side-skills">
       <h3>Skills</h3>
-      {!addSkillMode ? (
+      {mode.action == "add" ? (
+        <div className="add-skill">
+          <input className="input" id="skill-input" name="skill" type="text" />
+          <button onClick={saveNewSkill}>Save</button>
+          <button onClick={exitInput}>Cancel</button>
+        </div>
+      ) : mode.action == "edit" ? (
+        <div className="edit-skill">
+          <input
+            className="input"
+            id="skill-input"
+            name="skill"
+            type="text"
+            defaultValue={mode.skillName}
+          />
+          <button onClick={() => saveEditedSkill(mode.skillId)}>Save</button>
+          <button onClick={() => deleteSkill(mode.skillId)}>Delete</button>
+          <button onClick={exitInput}>Cancel</button>
+        </div>
+      ) : (
         <>
           {data.personalInfo.skills.map((skill) => (
-            <p key={skill.id} className="skill">
+            <p
+              key={skill.id}
+              className="skill"
+              data-skill-id={skill.id}
+              onClick={() => editSkill(skill.id, skill.skillName)}
+            >
               {skill.skillName}
             </p>
           ))}
-          <button onClick={handleAddSkill}>Add skill</button>
+          <button onClick={addSkill}>Add skill</button>
         </>
-      ) : (
-        <div className="skill">
-          <input className="input" id="new-skill" name="skill" type="text" />
-          <button onClick={handleSaveSkill}>Save</button>
-          <button onClick={handleCancelAddSkill}>Cancel</button>
-        </div>
       )}
     </div>
   );
