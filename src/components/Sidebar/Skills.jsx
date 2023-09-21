632be8564df5e7c9d21d8ab9deeb9d6ebc77d700
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-09-21 18:08:28 +0800

    Implement add skill functionality

--- src/components/Sidebar/Skills.jsx
+++ src/components/Sidebar/Skills.jsx
@@ -1,29 +1,58 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
 
 function Skills({ data, setData }) {
-  const [counter, setCounter] = useState(0);
-  const [newSkills, setNewSkills] = useState([]);
+  const [addSkillMode, setaddSkillMode] = useState(false);
 
   const handleAddSkill = () => {
-    setNewSkills((prevArray) => [...prevArray, counter]);
-    setCounter(counter + 1);
+    setaddSkillMode((prev) => !prev);
+  };
+
+  const handleCancelAddSkill = () => {
+    const newSkillInput = document.getElementById("new-skill");
+
+    newSkillInput.value = "";
+    setaddSkillMode((prev) => !prev);
+  };
+
+  const handleSaveSkill = () => {
+    const skillValue = document.getElementById("new-skill").value;
+    const skillId = uuidv4();
+
+    setData({
+      ...data,
+      personalInfo: {
+        ...data.personalInfo,
+        skills: [
+          ...data.personalInfo.skills,
+          { id: skillId, skillName: skillValue },
+        ],
+      },
+    });
+
+    handleCancelAddSkill();
   };
 
   return (
     <div className="side-skills">
       <h3>Skills</h3>
-      {data.personalInfo.skills.map((skill) => (
-        <p key={skill.id} className="skill">
-          {skill.skillName}
-        </p>
-      ))}
-      {/* {newSkills.map((skill) => (
-          <div key={skill} className="skill">
-            <input className="input" name="skill" type="text" />
-          </div>
-        ))} */}
-      <button onClick={handleAddSkill}>Add skill</button>
+      {!addSkillMode ? (
+        <>
+          {data.personalInfo.skills.map((skill) => (
+            <p key={skill.id} className="skill">
+              {skill.skillName}
+            </p>
+          ))}
+          <button onClick={handleAddSkill}>Add skill</button>
+        </>
+      ) : (
+        <div className="skill">
+          <input className="input" id="new-skill" name="skill" type="text" />
+          <button onClick={handleSaveSkill}>Save</button>
+          <button onClick={handleCancelAddSkill}>Cancel</button>
+        </div>
+      )}
     </div>
   );
 }
