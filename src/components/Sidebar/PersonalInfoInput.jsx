bad1bcab1efc096074116211769a3d00cb72a957
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-09-23 22:48:23 +0800

    Change input className

--- src/components/Sidebar/PersonalInfoInput.jsx
+++ src/components/Sidebar/PersonalInfoInput.jsx
@@ -16,77 +16,78 @@ function PersonalInfoInput({ data, setData }) {
   return (
     <div className="side-personal-info">
       <h3>Personal Information</h3>
-      <div className="input-group">
-        <label htmlFor="fullName">Full name</label>
-        <input
-          className="input"
-          name="fullName"
-          type="text"
-          defaultValue={data.personalInfo.fullName}
-          onChange={handleOnChange}
-        />
-      </div>
-      <div className="input-group">
-        <label htmlFor="birthDate">Birth date</label>
-        <input
-          className="input"
-          name="birthDate"
-          type="date"
-          defaultValue={data.personalInfo.birthDate}
-          onChange={handleOnChange}
-        />
-      </div>
-      <div className="input-group">
-        <label htmlFor="nationality">Nationality</label>
-        <input
-          className="input"
-          name="nationality"
-          type="text"
-          defaultValue={data.personalInfo.nationality}
-          onChange={handleOnChange}
-        />
-      </div>
-      <div className="input-group">
-        <label htmlFor="address">Address</label>
-        <input
-          className="input"
-          name="address"
-          type="text"
-          defaultValue={data.personalInfo.address}
-          onChange={handleOnChange}
-        />
-      </div>
-      <div className="input-group">
-        <label htmlFor="mobile">Mobile</label>
-        <input
-          className="input"
-          name="mobile"
-          type="text"
-          defaultValue={data.personalInfo.mobile}
-          onChange={handleOnChange}
-        />
-      </div>
-      <div className="input-group">
-        <label htmlFor="email">Email</label>
-        <input
-          className="input"
-          name="email"
-          type="text"
-          defaultValue={data.personalInfo.email}
-          onChange={handleOnChange}
-        />
-      </div>
-      <div className="input-group">
-        <label htmlFor="objective">Objective</label>
-        <textarea
-          className="input"
-          name="objective"
-          type="text"
-          rows="4"
-          defaultValue={data.personalInfo.objective}
-          onChange={handleOnChange}
-        ></textarea>
-      </div>
+      <>
+        <div className="input-group">
+          <label htmlFor="fullName">Full name</label>
+          <input
+            className="personal-info-input"
+            name="fullName"
+            type="text"
+            defaultValue={data.personalInfo.fullName}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="birthDate">Birth date</label>
+          <input
+            className="personal-info-input"
+            name="birthDate"
+            type="date"
+            defaultValue={data.personalInfo.birthDate}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="nationality">Nationality</label>
+          <input
+            className="personal-info-input"
+            name="nationality"
+            type="text"
+            defaultValue={data.personalInfo.nationality}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="address">Address</label>
+          <input
+            className="personal-info-input"
+            name="address"
+            type="text"
+            defaultValue={data.personalInfo.address}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="mobile">Mobile</label>
+          <input
+            className="personal-info-input"
+            name="mobile"
+            type="text"
+            defaultValue={data.personalInfo.mobile}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            className="personal-info-input"
+            name="email"
+            type="text"
+            defaultValue={data.personalInfo.email}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="objective">Objective</label>
+          <textarea
+            className="personal-info-input"
+            name="objective"
+            rows="4"
+            defaultValue={data.personalInfo.objective}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+      </>
     </div>
   );
 }
