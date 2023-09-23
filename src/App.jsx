ca65da2da5207d9f5b838e5a5381861a2b6c6f23
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-09-23 22:46:59 +0800

    Add sample data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Sidebar from "./components/Sidebar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [data, setData] = useState({
@@ -17,34 +18,52 @@ function App() {
       mobile: "+63 975 456 789",
       email: "juan@email.com",
       skills: [
-        { id: 1, skillName: "Skill1" },
-        { id: 2, skillName: "Skill2" },
+        { id: uuidv4(), skillName: "Skill1" },
+        { id: uuidv4(), skillName: "Skill2" },
       ],
       languages: [
-        { id: 3, languageName: "Language1" },
-        { id: 4, languageName: "Language2" },
+        { id: uuidv4(), languageName: "Language1" },
+        { id: uuidv4(), languageName: "Language2" },
       ],
     },
     workExperience: [
       {
+        id: uuidv4(),
         companyName: "Hooli Software",
         startDate: "2016-02-01",
         endDate: "2022-05-01",
         position: "Web Developer Trainee",
         specialization: "Software Development",
         industry: "Information Technology (Software)",
-        natureOfWork: [
-          "On-the-job training",
-          "Collaborates with a team in developing a web application",
-        ],
+        natureOfWork:
+          "On-the-job training, Collaborates with a team in developing a web application",
+      },
+      {
+        id: uuidv4(),
+        companyName: "FILAM Software Technology",
+        startDate: "2012-12-01",
+        endDate: "2016-08-01",
+        position: "Software Engineer",
+        specialization: "Software Development",
+        industry: "Information Technology (Software)",
+        natureOfWork:
+          "On-the-job training, Collaborates with a team in developing a web application",
       },
     ],
     education: [
       {
+        id: uuidv4(),
         attainment: "Bachelor's Degree",
         schoolName: "Holy Angel University",
         course: "BS in Computer Engineering",
         dateGraduated: "2015-06-10",
+      },
+      {
+        id: uuidv4(),
+        attainment: "High school diploma",
+        schoolName: "Cambridge University",
+        course: "STEM",
+        dateGraduated: "2010-04-10",
       },
     ],
   });
